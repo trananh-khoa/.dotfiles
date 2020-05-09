798c265052543911430f60c0ee1f8901f418313e
Khoa Tran <trananh_khoa@outlook.com>
2020-05-09 17:10:10 -0400

    Added java configuration script

---
+++ scripts/java.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+####################################################################################################
+# This script installs java packages and applications                                              #
+####################################################################################################
+
+# Install java
+
+brew install java
+
+# Install java applications
+
+brew cask install eclipse-jee       # Eclipse
+brew cask install intellij-idea-ce  # IntelliJ
